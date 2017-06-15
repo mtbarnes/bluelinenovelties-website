0813159c3f89d5df53c23cf4668b7cdd9a66a9f1
Pi <admin@bluelinenovelties.com>
2017-06-15 11:02:08 +0000

    deploy site shell script added

---
+++ deploy_site.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+cp -r css /var/www/html/
+cp -r fonts /var/www/html/
+cp -r img /var/www/html/
+cp index.html /var/www/html/
+cp -r js /var/www/html/
+cp -r rs-plugin /var/www/html/
