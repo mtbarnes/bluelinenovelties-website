8756b394a4b836c05725f7f39d2f417a121fd7a1
Marshall Barnes <mtb33@txstate.edu>
2017-07-22 00:14:30 -0500

    stupid wsgi

--- mainapp.py
+++ mainapp.py
@@ -7,6 +7,7 @@ working_dir = os.path.dirname(os.path.realpath(__file__))
 template_dir = os.path.join(working_dir, 'templates')
 
 app = Flask(__name__, template_folder=template_dir)
+application = app
 nav = Navigation(app)
 
 nav.Bar('top', [
