5bb04de70ca4efea4eca79663417879f0f7dfe9f
Marshall Barnes <mtb33@txstate.edu>
2017-07-25 02:22:14 -0500

    fuck it all

--- mainapp.py
+++ mainapp.py
@@ -22,7 +22,32 @@ nav.Bar('top', [
     nav.Item('Sign Up', 'signup')
     ])
 
-import views
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('intro.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/gallery')
+def gallery():
+    items = get_gallery_items()
+    return render_template('gallery.html', items=items)
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/philanthropy')
+def philanthropy():
+    return render_template('philanthropy.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,27 +1,2 @@
 from mainapp import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('intro.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/gallery')
-def gallery():
-    items = get_gallery_items()
-    return render_template('gallery.html', items=items)
-
-@app.route('/products')
-def products():
-    return render_template('products.html')
-
-@app.route('/philanthropy')
-def philanthropy():
-    return render_template('philanthropy.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
