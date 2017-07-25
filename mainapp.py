31bf391646257027e7fed5fa0eb55162de32e524
Marshall Barnes <mtb33@txstate.edu>
2017-07-25 03:50:20 -0500

    Revert "so fucking stupid, probably fixed import shit"
    This reverts commit dd260672d1642fda71aca6786a842024eb126c2c.

--- mainapp.py
+++ mainapp.py
@@ -10,7 +10,6 @@ template_dir = os.path.join(working_dir, 'templates')
 
 app = Flask(__name__, template_folder=template_dir)
 application = app
-from views import *
 
 nav = Navigation(app)
 
@@ -22,6 +21,34 @@ nav.Bar('top', [
     nav.Item('Philanthropy', 'philanthropy'),
     nav.Item('Sign Up', 'signup')
     ])
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('intro.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/gallery')
+def gallery():
+    items = get_gallery_items()
+    return render_template('gallery.html', items=items)
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/philanthropy')
+def philanthropy():
+    return render_template('philanthropy.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3535, host='0.0.0.0')
