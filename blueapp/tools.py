9717aa6c9adc2c601078c6be465afee01fc8d871
Marshall Barnes <mtb33@txstate.edu>
2017-07-25 00:55:06 -0500

    fuckass

---
+++ blueapp/tools.py
@@ -0,0 +1,43 @@
+def get_gallery_items():
+    items = []
+    items.append(dict([('name', 'Calcite in Limestone'),
+                       ('file', 'img/portfolio/large/01.jpg'),
+                       ('thumb', 'img/portfolio/thumb/01.jpg'),
+                       ('tags', ['calcite', 'crystals', 'limestone']),
+                       ('description', 'Beautiful calcite crystal from Jollyville, Texas.')]))
+    items.append(dict([('name', 'Small Calcite Crystals'),
+                       ('file', 'img/portfolio/large/02.jpg'),
+                       ('thumb', 'img/portfolio/thumb/02.jpg'),
+                       ('tags', ['calcite', 'crystals']),
+                       ('description', 'These samples clearly show the distinctive Calcite crystal lattice.')]))
+    items.append(dict([('name', 'UMOs'),
+                       ('file', 'img/portfolio/large/03.jpg'),
+                       ('thumb', 'img/portfolio/thumb/03.jpg'),
+                       ('tags', ['ufopoop']),
+                       ('description', 'Unidentified Metallic Objects found in the Slaughter Lane area.')]))
+    items.append(dict([('name', 'Metallic Meteorite'),
+                       ('file', 'img/portfolio/large/04.jpg'),
+                       ('thumb', 'img/portfolio/thumb/04.jpg'),
+                       ('tags', ['meteorites', 'ufopoop']),
+                       ('description', 'A dense metallic object thought by some to be a meteorite.')]))
+    items.append(dict([('name', 'Calcite on Limestone Matrix'),
+                       ('file', 'img/portfolio/large/05.jpg'),
+                       ('thumb', 'img/portfolio/thumb/05.jpg'),
+                       ('tags', ['calcite', 'crystals']),
+                       ('description', 'Beautiful Calcite cluster on a limestone matrix from Cedar Park, Texas')]))
+    items.append(dict([('name', 'UFO parts'),
+                       ('file', 'img/portfolio/large/06.jpg'),
+                       ('thumb', 'img/portfolio/thumb/06.jpg'),
+                       ('tags', ['meteorites', 'ufopoop']),
+                       ('description', 'Metallic objects of unknown origin, believed by some to be UFO parts.')]))
+    items.append(dict([('name', 'Fossilized Coral'),
+                       ('file', 'img/portfolio/large/07.jpg'),
+                       ('thumb', 'img/portfolio/thumb/07.jpg'),
+                       ('tags', ['fossils']),
+                       ('description', 'Fossils of aquatic origin found in the Lake Creek area.')]))
+    items.append(dict([('name', 'Unknown Fossil'),
+                       ('file', 'img/portfolio/large/08.jpg'),
+                       ('thumb', 'img/portfolio/thumb/08.jpg'),
+                       ('tags', ['fossils']),
+                       ('description', 'Unknown fossil, possibly aquatic, found near shells in north Austn.')]))
+    return items
