c1a25ceba908a801cb6b6c3701a4460acafaedc3
Marshall Barnes <mtb33@txstate.edu>
2017-07-28 03:50:57 -0500

    naming issues

--- blueapp/models.py
+++ blueapp/models.py
@@ -5,23 +5,24 @@ class GalleryItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     # User authentication information (required for Flask-User)
-    title = db.Column(db.Unicode(255), nullable=False, server_default=u'', unique=True)
+    name = db.Column(db.Unicode(255), nullable=False, server_default=u'', unique=True)
     description = db.Column(db.String(511), nullable=False, server_default='')
     # reset_password_token = db.Column(db.String(100), nullable=False, server_default='')
     active = db.Column(db.Boolean(), nullable=False, server_default='1')
     imagefile = db.Column(db.String(511), nullable=False, server_default='')
     thumbfile = db.Column(db.String(511), nullable=False, server_default='')
 
-    def __init__(self, title, description, imagefile, thumbfile):
-        self.title = title
+    def __init__(self, name, description, imagefile, thumbfile):
+        self.name = name
         self.description = description
         self.imagefile = imagefile
         self.thumbfile = thumbfile
 
     def __repr__(self):
-        return '<GalleryItem %r>' % self.title
+        return '<GalleryItem %r>' % self.name
 
 
 
 def get_gallery_items():
-    return GalleryItem.query.all()
+    items = GalleryItem.query.all()
+    return items
